4886bb3aa50892b2bcae41aebe574ff9cfaf99f4
Nguyễn Ngọc Bình <47907431+nguyenngocbinh@users.noreply.github.com>
2024-03-07 17:38:39 +0700

    update create job

--- ETL/scripts/create job.sql
+++ ETL/scripts/create job.sql
@@ -1,31 +1,38 @@
-USE msdb;
+/*
+-- Enable xp_cmdshell if not already enabled
+EXEC sp_configure 'show advanced options', 1;
+RECONFIGURE;
+EXEC sp_configure 'xp_cmdshell', 1;
+RECONFIGURE;
+*/
+
+USE [msdb];
 GO
 
--- Create a new job
+-- Schedule the execution of the stored procedure using SQL Server Agent
+DECLARE @jobId BINARY(16);
 EXEC dbo.sp_add_job
     @job_name = N'ETL_IFRS9_Data_Job',
-    @description = N'Job to execute ETL process for exchange rates';
+    @enabled = 1,
+    @description = N'Job to execute ETL process for exchange rates',
+    @job_id = @jobId OUTPUT;
 
--- Add a job step to run the Python script
 EXEC sp_add_jobstep
     @job_name = N'ETL_IFRS9_Data_Job',
     @step_name = N'Run ETL Script',
-    @subsystem = N'CmdExec',
-    @command = N'python E:\BinhNN2\ETL\scripts\etl_exchange_rate.py';
+    @subsystem = N'TSQL',
+    @command = N"exec xp_cmdshell 'E:BinhNN2\ETL\scripts\etl.bat'";
 
--- Schedule the job to run daily at a specific time (e.g., 2:00 AM)
 EXEC dbo.sp_add_schedule
     @schedule_name = N'ETL_Exchange_Rate_Schedule',
-    @freq_type = 4,
-    @freq_interval = 1,
-    @active_start_time = 20000;
+    @freq_type = 4, -- Daily
+    @freq_interval = 1, -- Every day
+    @active_start_time = 20000; -- 2:00 AM
 
--- Attach the schedule to the job
 EXEC dbo.sp_attach_schedule
     @job_name = N'ETL_IFRS9_Data_Job',
     @schedule_name = N'ETL_Exchange_Rate_Schedule';
 
--- Add the job to the SQL Server Agent
 EXEC dbo.sp_add_jobserver
     @job_name = N'ETL_IFRS9_Data_Job',
     @server_name = N'(local)';
