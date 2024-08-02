e63c3fe0a49dc128a2d52f3952387dc74ec3f546
Nguyễn Ngọc Bình <47907431+nguyenngocbinh@users.noreply.github.com>
2024-08-02 15:00:26 +0700

    rename function gini

--- Monitor/main.py
+++ Monitor/main.py
@@ -1,25 +1,25 @@
 import logging
-from monitor.retail import retail
-from monitor.nonRetail import nonRetail
-from monitor.retailCC import retailCC
-from monitor.retailMortgageV2 import retailMortgageV2
+from monitor.giniRetail import giniRetail
+from monitor.giniNonRetail import giniNonRetail
+from monitor.giniRetailCC import giniRetailCC
+from monitor.giniRetailMortgageV2 import giniRetailMortgageV2
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     logger.info("STARTING RETAIL PROCESSING...")
-    retail()
+    giniRetail()
     logger.info("RETAIL PROCESSING COMPLETED.")
 
     logger.info("STARTING NON-RETAIL PROCESSING...")
-    nonRetail()
+    giniNonRetail()
     logger.info("NON-RETAIL PROCESSING COMPLETED.")
 
     logger.info("STARTING RETAIL CREDITCARD PROCESSING...")
-    retailCC()
+    giniRetailCC()
     logger.info("RETAIL PROCESSING COMPLETED.")
 
     logger.info("STARTING RETAIL MORTGAGE V2 PROCESSING...")
-    retailMortgageV2()
+    giniRetailMortgageV2()
     logger.info("RETAIL MORTGAGE V2 PROCESSING COMPLETED.")
